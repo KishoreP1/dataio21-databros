7bb9a5d1f3ee1da0bb464f83fbf2cbb6b1fa8c0a
Adam G <37425558+aglucky@users.noreply.github.com>
2021-10-23 13:20:21 -0400

    Delete honda directory

--- honda/v2x_sdk/__init__.py
+++
@@ -1,4 +0,0 @@
-from v2x_sdk.sdk import get_dataframe, get_dictionary, get_json, status, get_auth_code, get_context
-from v2x_sdk.context import Context
-from v2x_sdk.constants import PUBLIC,SECRET,TOP_SECRET, SPAT,SUMMARY,DEVICE,EVTWARN,HOST,RVBSM,LOGIN_99P,LOGIN_KOTOMATIC, HEADERS
-from keycloak import KeycloakOpenID,exceptions
